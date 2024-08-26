7fda306089322bddadf9fddce76e1c20d49f0ed8
Tibor Claassen <tc@codebeige.net>
2024-08-26 18:51:54 +0200

    Fix plugin directory detection

--- lua/rig.lua
+++ lua/rig.lua
@@ -1,8 +1,18 @@
-local root_dir = vim.fs.root(debug.getinfo(1).source, "README.md")
+local _root_dir
+
+local function plugin_path(...)
+  local src = debug.getinfo(2, "S").short_src
+  _root_dir = _root_dir or vim.fs.root(src, "README.md")
+  assert(
+    _root_dir,
+    "Error resolving plugin path '" .. vim.fs.joinpath(...) .. "'"
+  )
+  return vim.fs.joinpath(_root_dir, ...)
+end
 
 local function require_fennel(src)
   package.preload["fennel"] = package.preload["fennel"] or loadfile(
-     src or vim.fs.joinpath(root_dir, "lib", "fennel.lua")
+     src or plugin_path("lib", "fennel.lua")
   )
   return require("fennel")
 end
@@ -40,8 +50,8 @@ local function setup()
   local fennel = require_fennel(vim.env.RIG_NVIM_FENNEL)
   insert_at(package.loaders, 2, fennel.searcher)
   local plugin_paths = {
-    vim.fs.joinpath(root_dir, "fnl", "?.fnl"),
-    vim.fs.joinpath(root_dir, "fnl", "?", "init.fnl"),
+    plugin_path("fnl", "?.fnl"),
+    plugin_path("fnl", "?", "init.fnl"),
   }
   for _, k in pairs({"path", "macro-path"}) do
     fennel[k] = prepend(fennel[k], unpack(plugin_paths))
